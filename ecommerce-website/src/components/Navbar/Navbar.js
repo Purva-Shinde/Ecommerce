455d47897b7e977659d41baaf8510ca0b43a52cf
Purva.Shinde <p.shinde@rnt.ai>
2024-06-19 19:29:15 +0530

    added navbar

--- ecommerce-website/src/components/Navbar/Navbar.js
+++ ecommerce-website/src/components/Navbar/Navbar.js
@@ -1,53 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
+import "./Navbar.css";
+import { Link } from 'react-router-dom';
 
-function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
 
-  const toggleNavbar = () => {
-    setIsOpen(!isOpen);
-  };
+const Navbar = () => {
+ 
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+ 
 
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl mx-auto p-4 flex flex-wrap items-center justify-between">
-        <a href="#" className="flex items-center">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-        </a>
-        <button
-          onClick={toggleNavbar}
-          type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded={isOpen ? "true" : "false"}
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-        </button>
-        <div className={`${isOpen ? "block" : "hidden"} w-full md:block md:w-auto`} id="navbar-default">
-          <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-            </li>
-          </ul>
+    <nav className = "navbar">
+      <div className='navbar-content'>
+        <div className = "container">
+          <div className = "navbar-top flex flex-between">
+              <Link to = "/" className = "navbar-brand">
+                <span className = "text-regal-blue">Shopping</span><span className='text-gold'>Hub.</span>
+              </Link>
+
+              <form className = "navbar-search flex">
+                <input type = "text" placeholder='Search here ...' />
+                <button type = "submit" className = "navbar-search-btn">
+                  <i className = "fas fa-search"></i>
+                </button>
+              </form>
+
+              <div className = "navbar-btns">
+                <Link to = "/cart" className="add-to-cart-btn flex">
+                  <span className = "btn-ico">
+                    <i className = "fas fa-shopping-cart"></i>
+                  </span>
+                  <div className='btn-txt fw-5'>Cart
+                    <span className='cart-count-value'>{ }</span>
+                  </div>
+                </Link>
+              </div>
+          </div>
+        </div>
+        
+        <div className='navbar-bottom bg-regal-blue'>
+          <div className='container flex flex-between'>
+            <ul className = {`nav-links flex ${isSidebarOpen ? 'show-nav-links' : ""}`}>
+              <button type = "button" className='navbar-hide-btn text-white' onClick={() => setIsSidebarOpen(false)}>
+                <i className='fas fa-times'></i>
+              </button>
+              {/* {
+                categories.map(category => (
+                  <li key = {category.id}><Link to = {`/category/${category.id}`} className = "nav-link text-white" onClick={() => setIsSidebarOpen(false)}>{category.name}</Link></li>
+                ))
+              } */}
+            </ul>
+
+            <button type = "button" className='navbar-show-btn text-gold' onClick={() => setIsSidebarOpen(true)}>
+              <i className = "fas fa-bars"></i>
+            </button>
+          </div>
         </div>
       </div>
     </nav>
-  );
+  )
 }
 
 export default Navbar;
