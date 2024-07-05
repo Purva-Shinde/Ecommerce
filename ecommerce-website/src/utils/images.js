d2066d68916af5d4898bae11bb03ed4e657c1dc3
Purva.Shinde <p.shinde@rnt.ai>
2024-07-05 17:01:22 +0530

    changes in hero section

--- ecommerce-website/src/utils/images.js
+++ ecommerce-website/src/utils/images.js
@@ -1,6 +1,6 @@
-import slider_img_1 from "../assets/images/slider-img-1.png";
-import slider_img_2 from "../assets/images/slider-img-2.png";
-import slider_img_3 from "../assets/images/slider-img-3.png";
+import slider_img_1 from "../assets/images/slider-img-1.jpg";
+import slider_img_2 from "../assets/images/slider-img-2.jpg";
+import slider_img_3 from "../assets/images/slider-img-3.jpg";
 import spinner from "../assets/images/spinner.svg";
 import error from "../assets/images/error.png";
 
